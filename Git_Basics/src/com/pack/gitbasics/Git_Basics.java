226bbe6ef66abd792ecff07e60afffd2b7f59ba7
Karthik0125 <karthikvaddadioff@gmail.com>
2023-04-07 16:45:31 +0530

    Changes that are made in the Git_Basics file.

--- Git_Basics/src/com/pack/gitbasics/Git_Basics.java
+++ Git_Basics/src/com/pack/gitbasics/Git_Basics.java
@@ -3,5 +3,7 @@ package com.pack.gitbasics;
 public class Git_Basics {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
+
+		System.out.println("These are the changes");
 	}
 }
